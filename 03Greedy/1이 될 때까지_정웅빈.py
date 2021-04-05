9693da2db4acee344fb62386f5514f2400db94bd
jwoongbin <69620190+jwoongbin@users.noreply.github.com>
2021-04-05 22:19:15 +0900

    Add files via upload

---
+++ 03Greedy/1이 될 때까지_정웅빈.py
@@ -0,0 +1,29 @@
+n, k = map(int, input().split())
+
+# count = 0
+# while n >= 2:
+#     if n % k == 0:
+#         n = n // k
+#         count += 1
+#     else:
+#         n = n - 1
+#         count += 1
+#
+# print(count)
+
+count = 0
+while True:
+
+    greedy = (n // k) * k
+    count += (n - greedy)
+
+    n = greedy
+
+    if n < k:
+        break
+
+    n //= k
+    count += 1
+    
+count += (n-1)
+print(count)
