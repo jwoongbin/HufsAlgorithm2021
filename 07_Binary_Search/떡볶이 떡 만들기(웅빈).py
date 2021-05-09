5368052b8690a0e3505f535f34348d8e12b45599
jwoongbin <69620190+jwoongbin@users.noreply.github.com>
2021-05-09 23:50:13 +0900

    Add files via upload

---
+++ 07_Binary_Search/떡볶이 떡 만들기(웅빈).py
@@ -0,0 +1,35 @@
+def cutting(array, m, start, end):
+
+    # 중간 값을 절단기의 높이 h로 설정
+    mid = (start + array[end]) // 2
+
+    # 절단된 떡의 길이의 합
+    sum = 0
+
+    for i in range(end, -1, -1):
+        if array[i] < mid:
+            break
+        else:
+            sum += array[i] - mid
+
+    # 절단된 떡의 길이의 합이 m 과 같을 경우
+    if sum == m:
+        return mid
+
+    # 절단된 떡의 길이의 합이 m 보다 클 경우
+    elif sum > m:
+        return cutting(array, m, mid, end)
+    # 절단된 떡의 길이의 합이 m 보다 작을 경우 시작점의 길이를 -1씩 감소
+    else:
+        return cutting(array, m, start-1, end)
+
+
+
+
+# 떡의 개수, 필요한 길이 입력 받기
+n,m = list(map(int, input().split()))
+array = list(map(int, input().split()))
+array.sort()
+print(cutting(array, m, 0, n-1))
+
+
---
+++ 07_Binary_Search/부품 찾기(웅빈).py
@@ -0,0 +1,34 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return mid
+
+        # 중간 값이 타켓보다 클 경우 왼쪽 탐색
+        elif array[mid] > target:
+            end = mid-1
+
+        # 중간 값이 타켓보다 작을 경우 오른쪽 탐색
+        else:
+            start = mid+1
+
+    return None
+
+
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+
+m = int(input())
+array2 = list(map(int, input().split()))
+
+
+for i in array2:
+
+        result = binary_search(array, i, 0, n-1)
+
+        if result == None:
+            print('no', end =' ')
+        else:
+            print('yes', end= ' ')
