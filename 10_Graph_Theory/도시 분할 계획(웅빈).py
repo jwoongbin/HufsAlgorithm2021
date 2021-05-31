dbd53566d326544b5e40b679c350d04dc4e7444d
jwoongbin <69620190+jwoongbin@users.noreply.github.com>
2021-05-31 22:46:32 +0900

    Create 도시 분할 계획(웅빈).py

---
+++ 10_Graph_Theory/도시 분할 계획(웅빈).py
@@ -0,0 +1,46 @@
+# 특정 원소가 속한 집합을 찾기(루트 노트를 찾아서 반환)
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기(union 연산)
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, n + 1):
+    parent[i] = i
+
+edges = []
+result = 0
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+edges.sort()
+last = 0
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        last = cost
+
+print(result-last)
